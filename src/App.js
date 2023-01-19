61a851ec857ff79dead6b295fee735d4c000933e
arun emmidi <djdjdjaaa999@gmail.com>
2023-01-19 13:51:19 +0530

    pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+// import Search from './api';
+import { useState } from 'react';
+import axios from 'axios'
 function App() {
+  const [data, setdata] = useState({})
+  const [location, setLoc] = useState('')
+
+
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=fbc35f84a2086b5837c7e7d7358a7368&units=metric`
+
+
+
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setdata(response.data)
+        console.log(response.data);
+      })
+
+      setLoc('');
+
+    }
+
+  }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <div className="search">
+
+        <input type="text" className='it' onChange={(e) => setLoc(e.target.value)} value={location} onKeyUp={search} placeholder="Enter Location" />
+
+      </div>
+
+
+      <div className="container">
+        <div className='city-details'>
+          <h1 className='city'>{data.name}</h1>
+
+          <h1 className='weather'> {data.main ? data.main.temp + "°C" : null}</h1>
+        </div>
+
+        
+
+
+        <div className='desc'>
+
+          { data.weather && data.weather[0].main?<span>{data.weather[0].main}</span>:null }
+          
+        </div>
+
+
+        <div className='cards'>
+
+
+          {data.main && data.main.feels_like ? <div className='card'>
+            {/* <h1 className='card-heading'>Feels <br/> Like</h1> */}
+            <h1 className='card-heading'> Feels Like</h1>
+            <p>{data.main.feels_like}°C</p>
+          </div> : null
+          }
+
+
+          {data.main && data.main.temp_max ? <div className='card'>
+            {/* <h1 className='card-heading'>Max<br/>Temp</h1> */}
+            <h1 className='card-heading'> Max-Temp</h1>
+            <p>{data.main.temp_max} °C</p>
+          </div> : null
+          }
+
+          {data.wind && data.wind.speed ? <div className='card'>
+            {/* <h1 className='card-heading'>Min <br/> Temp</h1> */}
+            <h1 className='card-heading'> Wind Speed</h1>
+            <p>{data.wind.speed}°C</p>
+          </div> : null
+
+          }
+
+
+
+
+
+
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
